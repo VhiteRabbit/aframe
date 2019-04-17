b2a120955596556d14cfe3145b50df744f43a048
Srile <florianisk@yahoo.com>
2019-04-17 17:06:19 +0200

    Add optional custom Logo to loadingScreen

--- src/core/scene/loadingScreen.js
+++ src/core/scene/loadingScreen.js
@@ -9,12 +9,13 @@ var getSceneCanvasSize;
 var ATTR_NAME = 'loading-screen';
 var LOADER_TITLE_CLASS = 'a-loader-title';
 
-module.exports.setup = function setup (el, getCanvasSize) {
+module.exports.setup = function setup(el, getCanvasSize) {
   sceneEl = el;
   getSceneCanvasSize = getCanvasSize;
   var loaderAttribute = sceneEl.hasAttribute(ATTR_NAME) ? styleParser.parse(sceneEl.getAttribute(ATTR_NAME)) : undefined;
   var dotsColor = loaderAttribute && loaderAttribute.dotsColor || 'white';
   var backgroundColor = loaderAttribute && loaderAttribute.backgroundColor || '#24CAFF';
+  var logoTexturePath = loaderAttribute && loaderAttribute.logoTexturePath || undefined;
   var loaderEnabled = loaderAttribute === undefined || loaderAttribute.enabled === 'true' || loaderAttribute.enabled === undefined; // true default
   var loaderScene;
   var sphereGeometry;
@@ -22,30 +23,53 @@ module.exports.setup = function setup (el, getCanvasSize) {
   var sphereMesh1;
   var sphereMesh2;
   var sphereMesh3;
+  var planeGeometry;
+  var planeMaterial;
+  var planeMesh;
+  var logoTextureLoader;
   var camera;
   var clock;
   var time;
   var render;
 
-  if (!loaderEnabled) { return; }
+  if (!loaderEnabled) {
+    return;
+  }
 
   // Setup Scene.
   loaderScene = new THREE.Scene();
   sphereGeometry = new THREE.SphereGeometry(0.20, 36, 18, 0, 2 * Math.PI, 0, Math.PI);
-  sphereMaterial = new THREE.MeshBasicMaterial({color: dotsColor});
+  sphereMaterial = new THREE.MeshBasicMaterial({
+    color: dotsColor
+  });
   sphereMesh1 = new THREE.Mesh(sphereGeometry, sphereMaterial);
   sphereMesh2 = sphereMesh1.clone();
   sphereMesh3 = sphereMesh1.clone();
   camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.0005, 10000);
   clock = new THREE.Clock();
   time = 0;
-  render = function () {
+  render = function() {
     sceneEl.renderer.render(loaderScene, camera);
     time = clock.getElapsedTime() % 4;
     sphereMesh1.visible = time >= 1;
     sphereMesh2.visible = time >= 2;
     sphereMesh3.visible = time >= 3;
   };
+
+  if (logoTexturePath) {
+    logoTextureLoader = new THREE.TextureLoader();
+    logoTextureLoader.load(logoTexturePath, logoTextureLoader => {
+      planeGeometry = new THREE.PlaneGeometry(10, 10, 1, 1);
+      planeMaterial = new THREE.MeshBasicMaterial({
+        map: logoTextureLoader,
+        transparent: true,
+        opacity: 1
+      });
+      planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+      planeMesh.position.set(0, 6, -15);
+      camera.add(planeMesh);
+    });
+  }
 
   loaderScene.background = new THREE.Color(backgroundColor);
   loaderScene.add(camera);
@@ -58,32 +82,38 @@ module.exports.setup = function setup (el, getCanvasSize) {
   setupTitle();
 
   // Delay 200ms to avoid loader flashes.
-  setTimeout(function () {
-    if (sceneEl.hasLoaded) { return; }
+  setTimeout(function() {
+    if (sceneEl.hasLoaded) {
+      return;
+    }
     resize(camera);
     titleEl.style.display = 'block';
-    window.addEventListener('resize', function () { resize(camera); });
+    window.addEventListener('resize', function() {
+      resize(camera);
+    });
     sceneEl.renderer.setAnimationLoop(render);
   }, 200);
 };
 
-module.exports.remove = function remove () {
+module.exports.remove = function remove() {
   window.removeEventListener('resize', resize);
-  if (!titleEl) { return; }
+  if (!titleEl) {
+    return;
+  }
   // Hide title.
   titleEl.style.display = 'none';
 };
 
-function resize (camera) {
+function resize(camera) {
   var embedded = sceneEl.hasAttribute('embedded');
   var size = getSceneCanvasSize(sceneEl.canvas, embedded, sceneEl.maxCanvasSize, sceneEl.is('vr-mode'));
   camera.aspect = size.width / size.height;
   camera.updateProjectionMatrix();
-   // Notify renderer of size change.
+  // Notify renderer of size change.
   sceneEl.renderer.setSize(size.width, size.height, false);
 }
 
-function setupTitle () {
+function setupTitle() {
   titleEl = document.createElement('div');
   titleEl.className = LOADER_TITLE_CLASS;
   titleEl.innerHTML = document.title;
